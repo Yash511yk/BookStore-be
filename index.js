b78d2ffb02f2506a9addffd2d84efc3e1f9737b7
Yash511yk <93113083+Yash511yk@users.noreply.github.com>
2024-09-08 16:24:59 +0530

    change FrontendURL

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express();
-const cors = require('cors')
+app.use(
+    cors({
+      origin: [process.env.FRONTEND_URL],
+      methods: ["POST"],
+      credentials: true,
+    })
+  );
 const port = process.env.PORT || 5000;
 
 // middlewear 
